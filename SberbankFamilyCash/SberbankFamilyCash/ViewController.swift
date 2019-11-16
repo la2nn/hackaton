b5f5d1220c8be20a6cd71df880176cbb7471991a
nnick <nnickspr@gmail.com>
2019-11-16 12:40:07 +0300

    Initial

---
+++ SberbankFamilyCash/SberbankFamilyCash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SberbankFamilyCash
+//
+//  Created by Николай Спиридонов on 16.11.2019.
+//  Copyright © 2019 nnick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
